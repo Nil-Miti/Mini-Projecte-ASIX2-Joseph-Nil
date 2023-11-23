509384633e2828f7a55c37a42d9758355d8bbf53
Josephbeta <144924099+Josephbeta@users.noreply.github.com>
2023-11-23 15:15:18 +0100

    Add files via upload

--- installBD.sh
+++ installBD.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
- #Para instalar el paquete, como siempre primero actualizamos los repositorios y luego procedemos a instalar el siguiente paquete:
+#Para instalar el paquete, como siempre primero actualizamos los repositorios y luego procedemos a instalar el siguiente paquete:
 
-sudo apt update
-sudo apt install mariadb-server -y
+apt update
+apt install mariadb-server -y
 
 #Tras la instalación, procedemos a habilitar el servicio al arranque y lo iniciamos:
 
-sudo systemctl start mariadb.service
+systemctl start mariadb.service
 
 # Instalar gnupg2
-sudo apt install gnupg2 -y
+apt install gnupg2 -y
 
 # Descargar la clave pública de MongoDB
 wget -nc https://www.mongodb.org/static/pgp/server-6.0.asc
@@ -22,12 +22,11 @@ cat server-6.0.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/mongodb.gpg
 echo "deb [ arch=amd64,arm64 signed-by=/etc/apt/trusted.gpg.d/mongodb.gpg] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongo.list
 
 # Actualizar la lista de paquetes
-sudo apt update
+apt update
 
 # Instalar MongoDB
-sudo apt install mongodb-org -y
+apt install mongodb-org -y
 
 # Iniciar el servicio de MongoDB
-sudo systemctl start mongod
-
+systemctl start mongod
 
