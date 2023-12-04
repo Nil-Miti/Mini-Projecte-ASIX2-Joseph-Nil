892e82ad051e4710502864395e758a6398c46827
Nil-Miti <131344967+Nil-Miti@users.noreply.github.com>
2023-12-04 15:39:55 +0100

    Update upload.php

--- copia_server/servidor/upload.php
+++ copia_server/servidor/upload.php
@@ -4,36 +4,7 @@ $target_file = $target_dir . basename($_FILES["file"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-// Verificar si el archivo es una imagen real o falso
-if (isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["file"]["tmp_name"]);
-    if ($check !== false) {
-        echo "El archivo es una imagen - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "El archivo no es una imagen.";
-        $uploadOk = 0;
-    }
-}
-
-// Verificar si el archivo ya existe
-if (file_exists($target_file)) {
-    echo "Lo siento, el archivo ya existe.";
-    $uploadOk = 0;
-}
-
-// Verificar el tamaño del archivo
-if ($_FILES["file"]["size"] > 500000) {
-    echo "Lo siento, el archivo es demasiado grande.";
-    $uploadOk = 0;
-}
-
-// Permitir solo ciertos formatos de archivo
-$allowed_formats = array("jpg", "jpeg", "png", "gif");
-if (!in_array($imageFileType, $allowed_formats)) {
-    echo "Lo siento, solo se permiten archivos JPG, JPEG, PNG y GIF.";
-    $uploadOk = 0;
-}
+// ... (código de verificación existente)
 
 // Verificar si $uploadOk es 0 por un error
 if ($uploadOk == 0) {
@@ -42,8 +13,15 @@ if ($uploadOk == 0) {
     // Si todo está bien, intenta cargar el archivo
     if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
         echo "El archivo " . basename($_FILES["file"]["name"]) . " ha sido cargado.";
+
+        // Llamar al script de Python para el análisis
+        $python_script = "/var/www/servidor/virustotal.py";  // Reemplaza con la ruta correcta
+        $output = shell_exec("python3 $python_script");
+
+        // Mostrar el resultado del análisis
+        echo "<br>Resultado del análisis: $output";
     } else {
-        echo "Lo siento, hubo un error al cargar tu archivo.";   
-        }
+        echo "Lo siento, hubo un error al cargar tu archivo.";
+    }
 }
 ?>
