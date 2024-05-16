cad12879714e32c86b38dc4f9f07b88f6920ac93
Josephbeta <144924099+Josephbeta@users.noreply.github.com>
2024-05-16 18:46:26 +0200

    Add files via upload

---
+++ copiafinal_fase1/admin/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+   $('#section1').DataTable();
+});
+
+$(document).ready(function() {
+   $('#section2').DataTable();
+});
+
+$(document).ready(function() {
+   $('#section3').DataTable();
+});
